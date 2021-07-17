4ca45a38444a4e11caa25e9785f46d371684f2a1
douglasmatosdev <douglasmatosdev@gmail.com>
2021-07-17 20:58:44 -0300

    Exe07

--- src/com/loiane/cursojava/aula13/labs/Exer07.java
+++ src/com/loiane/cursojava/aula13/labs/Exer07.java
@@ -1,2 +1,21 @@
-package com.loiane.cursojava.aula13.labs;public class Exer07 {
+package com.loiane.cursojava.aula13.labs;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exer07 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Inform o tamanho do lado do quadrado:");
+        double lado = sc.nextDouble();
+
+        // área = lado * lado
+        double area = Math.pow(lado, 2);
+
+        System.out.println("A área do quadrado é :" + area);
+        System.out.println("O dobro da área do quadrado é :" + area * 2);
+    }
 }
