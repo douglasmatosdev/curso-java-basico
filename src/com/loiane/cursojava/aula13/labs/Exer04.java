66383bbdd7b5cd2c24748f61b5bf3f57023db0c6
douglasmatosdev <douglasmatosdev@gmail.com>
2021-07-17 20:58:20 -0300

    Exe04

--- src/com/loiane/cursojava/aula13/labs/Exer04.java
+++ src/com/loiane/cursojava/aula13/labs/Exer04.java
@@ -1,2 +1,28 @@
-package com.loiane.cursojava.aula13.labs;public class Exer04 {
+package com.loiane.cursojava.aula13.labs;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exer04 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Informe a primeira nota:");
+        double nota1 = scanner.nextDouble();
+
+        System.out.println("Informe a segunda nota:");
+        double nota2 = scanner.nextDouble();
+
+        System.out.println("Informe a terceira nota:");
+        double nota3 = scanner.nextDouble();
+
+        System.out.println("Informe a quarta nota:");
+        double nota4 = scanner.nextDouble();
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("A média das notas é: " + media);
+    }
 }
