6fd3257134f9f08184b5c875f979f5e9d3728946
douglasmatosdev <douglasmatosdev@gmail.com>
2021-07-17 19:23:18 -0300

    mais variáveis e escape

---
+++ src/com/loiane/cursojava/aula11/VariaveisFlutuante.java
@@ -0,0 +1,14 @@
+package com.loiane.cursojava.aula11;
+
+public class VariaveisFlutuante {
+
+    public static void main(String[] args) {
+        double valorDaPassagem = 2.90;
+
+        float valorTomate = 3.95f;
+
+        System.out.println("Valor da passagem = " + valorDaPassagem);
+
+        System.out.println("Valor do tomate = " + valorTomate);
+    }
+}
