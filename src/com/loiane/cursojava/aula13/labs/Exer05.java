390d029978703e45ac4026ca1c80de2cc42d15bc
douglasmatosdev <douglasmatosdev@gmail.com>
2021-07-17 20:58:27 -0300

    Exe05

--- src/com/loiane/cursojava/aula13/labs/Exer05.java
+++ src/com/loiane/cursojava/aula13/labs/Exer05.java
@@ -1,2 +1,20 @@
-package com.loiane.cursojava.aula13.labs;public class Exer05 {
+package com.loiane.cursojava.aula13.labs;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exer05 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Informe um valor em metros:");
+        double metros = sc.nextDouble();
+
+        // 1m = 100cm
+        double cm = metros * 100;
+
+        System.out.println(metros + "m é igual a " + cm + "cm");
+    }
 }
