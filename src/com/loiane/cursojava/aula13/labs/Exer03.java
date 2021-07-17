38fc008c9179269f9a1e24f7b69cbe87c424455b
douglasmatosdev <douglasmatosdev@gmail.com>
2021-07-17 20:58:14 -0300

    Exe03

--- src/com/loiane/cursojava/aula13/labs/Exer03.java
+++ src/com/loiane/cursojava/aula13/labs/Exer03.java
@@ -1,2 +1,21 @@
-package com.loiane.cursojava.aula13.labs;public class Exer03 {
+package com.loiane.cursojava.aula13.labs;
+
+import java.util.Scanner;
+
+public class Exer03 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int primeiroNumero = scanner.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        int segundoNumero = scanner.nextInt();
+
+        int resultado = primeiroNumero + segundoNumero;
+
+        System.out.println("A soma dos valores informados é: " + resultado);
+    }
 }
