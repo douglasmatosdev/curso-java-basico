09db9ac746f63af152d8cb823c9229dee7ebebc3
douglasmatosdev <douglasmatosdev@gmail.com>
2021-07-17 21:07:20 -0300

    Exer10

--- src/com/loiane/cursojava/aula13/labs/Exer10.java
+++ src/com/loiane/cursojava/aula13/labs/Exer10.java
@@ -1,2 +1,19 @@
-package com.loiane.cursojava.aula13.labs;public class Exer10 {
+package com.loiane.cursojava.aula13.labs;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exer10 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Inform uma temperatura em Celsius: ");
+        double celsius = sc.nextDouble();
+
+        double farenheit = (celsius * 1.8) + 32;
+
+        System.out.println("A temperatura " + celsius +" C é igual a " + farenheit + " F");
+    }
 }
