cd66c8bc74b7b3f90ded3d265dafcefed5a82078
douglasmatosdev <douglasmatosdev@gmail.com>
2022-03-01 10:36:57 -0300

    resolved exercises 01, 02 and 03

---
+++ src/com/douglas/cursojava/exercicios/Exe01.java
@@ -0,0 +1,16 @@
+package com.douglas.cursojava.exercicios;
+
+/**
+ * 
+ * @author doug
+ *
+ * @implNote Faça um programa que mostre a mensagem "Alo mundo"na tela.
+ */
+public class Exe01 {
+
+	public static void main(String[] args) {
+		System.out.println("Alo mundo");
+
+	}
+
+}
